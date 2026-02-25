6a812cc068358566ffc4be3cc59bc52cd667474c
ShahJahanBro <shahjahan12098@gmail.com>
2026-02-26 00:01:26 +0500

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Navbar  from './components/Navbar';
 import Home from './pages/Home';
+import About   from './pages/About';
 
 /* Scroll to top on every route change */
 function ScrollTop() {
@@ -18,7 +19,7 @@ export default function App() {
 
        <Routes>
         <Route path="/"               element={<Home />}           />
-        
+        <Route path="/about"          element={<About />}        />
       </Routes>
 
     </BrowserRouter>
